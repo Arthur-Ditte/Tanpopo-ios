53ec3774ff64bccad4d894ff735d77232317213a
RestytheCake <morganischkalt@gmail.com>
2024-10-05 19:40:59 +0200

    Initial Commit

---
+++ Tanpopo/TanpopoApp.swift
@@ -0,0 +1,32 @@
+//
+//  TanpopoApp.swift
+//  Tanpopo
+//
+//  Created by Arthur Ditte on 05.10.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TanpopoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
